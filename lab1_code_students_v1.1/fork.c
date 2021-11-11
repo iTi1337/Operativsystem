a0230a0dbf231b2beeb7434b76565830aff88882
iTi1337 <64363659+iTi1337@users.noreply.github.com>
2021-11-11 16:48:38 +0100

    fixed the first one

--- lab1_code_students_v1.1/fork.c
+++ lab1_code_students_v1.1/fork.c
@@ -18,7 +18,7 @@ int main(int argc, char **argv)
             for (i = 0; i < niterations; ++i)
             printf("B = %d, ", i);
         } else{
-            printf("Pid child 1: %d\n Pid child 2: %d\n", pid, pid2);
+            printf("Pid child 1: %d\nPid child 2: %d\n", pid, pid2);
             for (i = 0; i < niterations; ++i)
                 printf("C = %d, ", i);
         }
@@ -27,5 +27,10 @@ int main(int argc, char **argv)
 }
 
 /*
-the fork.c
+The variableiis used in both processes. Is the value ofiin one process affected when the other process incrementsit? Why/Why not?
+
+Since a fork creates a separate copy of the current environment, both processes having separate i's.
+Since a fork() can't send info back, opposed to a clone().
+
+While executing the code, we got Pid 2968 for child 1 and 2969 for child 2
 */
