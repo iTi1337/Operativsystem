2ea9cc8facffcc326353d9be5458f7b0e9140913
iTi1337 <64363659+iTi1337@users.noreply.github.com>
2021-11-11 16:20:31 +0100

    pid2

--- lab1_code_students_v1.1/fork.c
+++ lab1_code_students_v1.1/fork.c
@@ -4,17 +4,24 @@
 int main(int argc, char **argv)
 {
     pid_t pid;
+    pid_t pid2;
     unsigned i;
     unsigned niterations = 1000;
     pid = fork();
+    
     if (pid == 0) {
-        
         for (i = 0; i < niterations; ++i)
             printf("A = %d, ", i);
      } else {
-        printf("%d", pid);
-        for (i = 0; i < niterations; ++i)
+        pid2 = fork();
+        if (pid2 == 0){
+            for (i = 0; i < niterations; ++i)
             printf("B = %d, ", i);
+        } else{
+            printf("Pid child 1: %d\n Pid child 2: %d\n", pid, pid2);
+            for (i = 0; i < niterations; ++i)
+                printf("C = %d, ", i);
+        }
     }
     printf("\n");
 }
