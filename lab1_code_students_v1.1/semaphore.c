f9093f02299c85185c85a13eefbf4c152743f9b0
jesperls <jesper.ls@hotmail.com>
2021-11-12 14:47:24 +0100

    Co-authored-by: iTi1337 <iTi1337@users.noreply.github.com>

--- lab1_code_students_v1.1/semaphore.c
+++ lab1_code_students_v1.1/semaphore.c
@@ -11,16 +11,16 @@ const char *semName2 = "my_sema2";
 int main(int argc, char **argv)
 {
 	pid_t pid;
-	sem_t *sem_id1 = sem_open(semName1, O_CREAT, O_RDWR, 1);
-	sem_t *sem_id2 = sem_open(semName2, O_CREAT, O_RDWR, 0);
+	sem_t *sem_id1 = sem_open(semName1, O_CREAT, O_RDWR, 1); //open, initial value 1
+	sem_t *sem_id2 = sem_open(semName2, O_CREAT, O_RDWR, 0); //closed, initial value 2
 	int i, status;
 
 	pid = fork();
-	if (pid) {
+	if (pid) { //parent
 		for (i = 0; i < 100; i++) {
-			sem_wait(sem_id1);
+			sem_wait(sem_id1); //wait for open nr1
 			putchar('A'); fflush(stdout);
-			sem_post(sem_id2);
+			sem_post(sem_id2); //open nr2
 		}
 		sem_close(sem_id1);
 		sem_close(sem_id2);
@@ -29,9 +29,9 @@ int main(int argc, char **argv)
 		sem_unlink(semName2);
 	} else {
 		for (i = 0; i < 100; i++) {
-			sem_wait(sem_id2);
+			sem_wait(sem_id2); //wait for parent to post sem 2
 			putchar('B'); fflush(stdout);
-			sem_post(sem_id1);
+			sem_post(sem_id1); //opens sem 1 for parent
 		}
 		sem_close(sem_id1);
 		sem_close(sem_id2);
--- lab1_code_students_v1.1/shmem.c
+++ lab1_code_students_v1.1/shmem.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
@@ -10,40 +11,47 @@
 int main(int argc, char **argv)
 {
 	struct shm_struct {
-		int buffer;
-		unsigned empty;
+		int buffer[10];
+		unsigned empty[10];
 	};
 	volatile struct shm_struct *shmp = NULL;
 	char *addr = NULL;
 	pid_t pid = -1;
-	int var1 = 0, var2 = 0, shmid = -1;
+	int var1 = 0, var2 = 0,  shmid = -1;
 	struct shmid_ds *shm_buf;
 
 	/* allocate a chunk of shared memory */
 	shmid = shmget(IPC_PRIVATE, SHMSIZE, IPC_CREAT | SHM_R | SHM_W);
-	shmp = (struct shm_struct *) shmat(shmid, addr, 0);
-	shmp->empty = 0;
+	shmp = (struct shm_struct *) shmat(shmid, addr, 0); //shmid is a pointer to the shared struct
+	for(int i = 0; i < sizeof(shmp->empty) / sizeof(unsigned); ++i){
+		shmp->empty[i] = 0;
+	} // Fills the empty array with 0's
 	pid = fork();
 	if (pid != 0) {
 		/* here's the parent, acting as producer */
 		while (var1 < 100) {
 			/* write to shmem */
-			var1++;
-			while (shmp->empty == 1); /* busy wait until the buffer is empty */
+			while (shmp->empty[var1 % 10] == 1); /* busy wait until the buffer is empty */
 			printf("Sending %d\n", var1); fflush(stdout);
-			shmp->buffer = var1;
-			shmp->empty = 1;
+			shmp->buffer[var1 % 10] = var1; //adds to the buffer in a circular way
+			shmp->empty[var1 % 10] = 1; //adds to empty in a circular way
+			var1++;
+			double a = (rand() % 100) / 50.0; //generates a random number between 0 and 2
+			sleep(a);
 		}
 		shmdt(addr);
 		shmctl(shmid, IPC_RMID, shm_buf);
 	} else {
 		/* here's the child, acting as consumer */
-		while (var2 < 100) {
+		while (var2 < 100){
 			/* read from shmem */
-			while (shmp->empty == 0); /* busy wait until there is something */
-			var2 = shmp->buffer;
-			shmp->empty = 0;
+			while (shmp->empty[var2 % 10] == 0); /* busy wait until there is something */
+			var2 = shmp->buffer[var2 % 10]; //loads from buffer in a circular way
+			shmp->empty[var2 % 10] = 0; //removes from empty in a circular way
 			printf("Received %d\n", var2); fflush(stdout);
+			var2++;
+			double a = (rand() % 100) / 50.0;
+			sleep(a);
 		}
 		shmdt(addr);
 		shmctl(shmid, IPC_RMID, shm_buf);
