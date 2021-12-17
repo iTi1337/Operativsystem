80d87225ef3bed3e29dfaec92b8457f3e3482260
jesperls <jesper.ls@hotmail.com>
2021-12-17 01:16:48 +0100

    Changed comments and fixed small read bug

--- lab3_code_students_v1.1/fs.cpp
+++ lab3_code_students_v1.1/fs.cpp
@@ -16,9 +16,10 @@ FS::~FS()
 }
 
 //HELPER FUNCTIONS HELP ME FALL ASLEEP AT NIGHT
+//Takes in fat starting position and returns all fat indexes it finds untill it finds a -1 (EOF)
 std::vector<int>
-FS::get_fats(int first_blk){ // Takes in fat starting position and returns all fat indexes it finds untill it finds a -1 (EOF)
-    std::vector<int> return_values;
+FS::get_fats(int first_blk){ 
+    std::vector<int> return_values; //vectors are cool
     int pointer = first_blk;
     while (fat[pointer] != -1) {
         return_values.push_back(pointer);
@@ -29,12 +30,13 @@ FS::get_fats(int first_blk){ // Takes in fat starting position and returns all f
 }
 
 //LEET GAMER CODE
+//Takes in the first block of a file and returns the entire file, even if it's stored on multiple blocks
 std::string 
 FS::diskread(int first_blk){
     std::vector<int> to_read = get_fats(first_blk); //Gets all fat blocks to read from
     std::string total_read; //Where read values are stored
     for(int i = 0; i < to_read.size(); i++){//Reads one block and appends to output
-        char data[4096] = {0}; 
+        char data[4097] = {0};  //One extra bit to store null terminator
         disk.read(to_read[i], (uint8_t*)data);
         total_read.append(data);
     }
@@ -46,40 +48,41 @@ FS::diskread(int first_blk){
 int
 FS::diskwrite(std::string input, int start_blk = 0){
     int size = input.length();
-    int blocks = (int)(size/4096); //number of total blocks
-    int fat_indexes[blocks+1];
+    int blocks = (int)(size/4096); //Counts how many blocks to write to
+    int fat_indexes[blocks+1]; //How many indexes that are necessary
     int i = 0;
     if (start_blk != 0){ //If we specify block to start at
         i = 1; //Since start blk is set, we skip i = 0 in the loop
         fat_indexes[0] = start_blk;
         std::vector<int> to_reset = get_fats(start_blk);
-        for(int j = 0; j < to_reset.size();j++){ //Resets blocks which the block we started at points at
+        for(int j = 0; j < to_reset.size();j++){ //Nulls blocks which the block we started at points at
             fat[to_reset[j]] = 0;
         }
     }
     for (i; i <= blocks; i++){ //Looks for empty fat indexes for each block
         for (int j = 0; j < disk.get_no_blocks(); j++){
-            if(fat[j] == 0){ //if empty
-                fat_indexes[i] = j; //Saves empty FAT index
+            if(fat[j] == 0){
+                fat_indexes[i] = j; //Saves found empty FAT index
                 fat[j] = -1;
                 break;
             }
         }
     }
-    // may or may not work
-    // works
-    for (int i = 0; i <= blocks; i++){ //number of blocks needed
+
+    //THIS MIGHT BE WHERE WE WOULD WRITE CODE TO CHECK IF DISK IS FULL, AKA DID NOT FIND ENOUGH EMPTY FATS, THAT'S A JOB FOR LATER THOUGH
+    
+    for (int i = 0; i <= blocks; i++){ //Loops for every block necessary to write to
         std::string partition = "";
         if (i == blocks){ 
             //IF LAST BLOCK
             fat[fat_indexes[i]] = -1; //EOF
-            partition.append(input.substr(4096*i, size % 4096));
+            partition.append(input.substr(4096*i, size % 4096)); //Writes the last bits of the ipnut to partition
             partition.resize(4096); //To get rid of junk already stored in block
             disk.write(fat_indexes[i], (uint8_t*)partition.c_str());
         }
         else{
             fat[fat_indexes[i]] = fat_indexes[i+1]; //Points towards next fat index
-            partition.append(input.substr(4096*i, 4096));
+            partition.append(input.substr(4096*i, 4096)); //Since it's not the last block, we know that more than 4096 bytes can be written
             disk.write(fat_indexes[i], (uint8_t*)partition.data());
         }
     }
@@ -91,13 +94,12 @@ FS::diskwrite(std::string input, int start_blk = 0){
 int
 FS::format()
 {
-    uint8_t *blk = (uint8_t*)calloc(BLOCK_SIZE, sizeof(uint8_t*)); //writes a whole empty block
-    disk.write(0, blk); //time to write some cooode
-    memset(fat, 0, sizeof(fat));
-    fat[0] = -1;
+    uint8_t *blk = (uint8_t*)calloc(BLOCK_SIZE, sizeof(uint8_t*)); //Creates an empty block
+    disk.write(0, blk); //Writes it to the root directory
+    memset(fat, 0, sizeof(fat)); //Resets the local fat variable
+    fat[0] = -1; //Sets initial values
     fat[1] = -1;
-    disk.write(1, (uint8_t*)fat); //time to write some cooode
-    //how do i get a create disk function hmmm
+    disk.write(1, (uint8_t*)fat); //Writes local fat to the disk
     std::cout << "FS::format()\n";
     return 0;
 }
@@ -107,30 +109,30 @@ FS::format()
 int
 FS::create(std::string filepath) // /name_of_file
 {   
-    std::string input2 = "";
-    //char input[4096] = {'0'};
-    int first = 1;
-    int size = 0;
-    std::string input3;
-    while (getline(std::cin, input3, '\n')){
-        if (input3.empty())
+    std::string input = ""; //Variable for the entire input
+    int first = 1; //To check if it's the first line of input
+    std::string line_input; //To read new lines
+    while (getline(std::cin, line_input, '\n')){
+        if (line_input.empty())
             break;
         else{
             if(first != 1){ //if it's ugly and it's working, is it stil- yes, yes it is.
-                input2.append("\n");
+                input.append("\n");
             }
             first = 0;
-            input2.append(input3);
+            input.append(line_input);
         }
     }
+    //Creates the new directory entry
     dir_entry *new_file;
     new_file = (dir_entry*)calloc(1, sizeof(dir_entry));
     strcpy(new_file->file_name, filepath.c_str());
-    new_file->size = input2.length();
-    new_file->first_blk = diskwrite(input2);
+    new_file->size = input.length();
+    new_file->first_blk = diskwrite(input); //Writes the input to the disk and store where it begins
     
-    dir_entry *blk = (dir_entry*)calloc(disk.get_no_blocks(), sizeof(dir_entry)); //new_empty block
-    disk.read(0, (uint8_t*)blk); //should work tm
+    dir_entry *blk = (dir_entry*)calloc(disk.get_no_blocks(), sizeof(dir_entry)); //Creates an empty block
+    disk.read(0, (uint8_t*)blk); //Reads in the root directory
+    //Finds a spot for the file in the directory
     // DOES NOT CHECK FOR DUPLICATES, SHOULD PROBABLY BE FIXED EVENTUALLY
     for (int j = 0; j < 64; j++){
         if (strcmp(blk[j].file_name, "") == 0){
@@ -147,10 +149,10 @@ FS::create(std::string filepath) // /name_of_file
 int
 FS::cat(std::string filepath)
 {
-    dir_entry *blk = (dir_entry*)calloc(disk.get_no_blocks(), sizeof(dir_entry)); //new_empty block
-    disk.read(0, (uint8_t*)blk); //should work tm
+    dir_entry *blk = (dir_entry*)calloc(disk.get_no_blocks(), sizeof(dir_entry)); //Creates an empty block
+    disk.read(0, (uint8_t*)blk); //Reads in the root directory
     for (int i = 0; i < 64; i++){
-        if (strcmp(blk[i].file_name, filepath.c_str()) == 0){
+        if (strcmp(blk[i].file_name, filepath.c_str()) == 0){ //Finds the file with the inputted filepath
             std::string content;
             dir_entry file = blk[i];
             content.append(diskread(file.first_blk));
@@ -167,11 +169,11 @@ int
 FS::ls()
 {
     std::cout << "FS::ls()\n";
-    dir_entry *blk = (dir_entry*)calloc(disk.get_no_blocks(), sizeof(dir_entry)); //new_empty block
-    disk.read(0, (uint8_t*)blk); //should work tm
+    dir_entry *blk = (dir_entry*)calloc(disk.get_no_blocks(), sizeof(dir_entry)); //Creates an empty block
+    disk.read(0, (uint8_t*)blk); //Reads in the root directory
     std::cout << "name" << "   " << "size" << "\n";
     for (int i = 0; i < 64; i++){
-        if (strcmp(blk[i].file_name, "") != 0){
+        if (strcmp(blk[i].file_name, "") != 0){ //List every filename in directory which is not null
             std::cout << blk[i].file_name << "   " << blk[i].size << "\n";
         }
     }
@@ -184,22 +186,22 @@ int
 FS::cp(std::string sourcepath, std::string destpath)
 {
     std::cout << "FS::cp(" << sourcepath << "," << destpath << ")\n";
-    dir_entry *blk = (dir_entry*)calloc(disk.get_no_blocks(), sizeof(dir_entry)); //new_empty block to store root dir in
-    disk.read(0, (uint8_t*)blk); //should work tm
+    dir_entry *blk = (dir_entry*)calloc(disk.get_no_blocks(), sizeof(dir_entry)); //Creates an empty block
+    disk.read(0, (uint8_t*)blk); //Reads in the root directory
     for (int i = 0; i < 64; i++){
-        if (strcmp(blk[i].file_name, sourcepath.c_str()) == 0){
+        if (strcmp(blk[i].file_name, sourcepath.c_str()) == 0){ //finds the file with the source name
+            //Creates the new directory entry
             dir_entry *new_file;
             new_file = (dir_entry*)malloc(sizeof(dir_entry));
             strcpy(new_file->file_name, destpath.c_str());
             new_file->size = blk[i].size;
             new_file->first_blk = diskwrite(diskread(blk[i].first_blk)); //Reads the data from the old file, writes the data from the new file and saves where it starts, how neat aint that (genuint stolt över detta)
-
-            dir_entry *new_blk = (dir_entry*)calloc(disk.get_no_blocks(), sizeof(dir_entry)); //new_empty block
-            disk.read(0, (uint8_t*)new_blk); //should work tm
+            //Finds a spot for the file in the directory
+            // DOES NOT CHECK FOR DUPLICATES, SHOULD PROBABLY BE FIXED EVENTUALLY
             for (int k = 0; k < 64; k++){
-                if (strcmp(new_blk[k].file_name, "") == 0){
-                    new_blk[k] = *new_file;
-                    disk.write(0, (uint8_t*)new_blk);
+                if (strcmp(blk[k].file_name, "") == 0){
+                    blk[k] = *new_file;
+                    disk.write(0, (uint8_t*)blk);
                     break;
                 }
             }
@@ -215,12 +217,12 @@ int
 FS::mv(std::string sourcepath, std::string destpath)
 {
     std::cout << "FS::mv(" << sourcepath << "," << destpath << ")\n";
-    dir_entry *blk = (dir_entry*)calloc(disk.get_no_blocks(), sizeof(dir_entry)); //new_empty block
-    disk.read(0, (uint8_t*)blk); //should work tm
+    dir_entry *blk = (dir_entry*)calloc(disk.get_no_blocks(), sizeof(dir_entry)); //Creates an empty block
+    disk.read(0, (uint8_t*)blk); //Reads in the root directory
     for (int i = 0; i < 64; i++){
-        if (strcmp(blk[i].file_name, sourcepath.c_str()) == 0){
-            strcpy(blk[i].file_name, destpath.c_str());
-            disk.write(0, (uint8_t*)blk);
+        if (strcmp(blk[i].file_name, sourcepath.c_str()) == 0){ //Finds the source file
+            strcpy(blk[i].file_name, destpath.c_str()); //Changes its name to specified name
+            disk.write(0, (uint8_t*)blk); //Writes the change to the disk
             break;
         }
     }
@@ -232,16 +234,15 @@ int
 FS::rm(std::string filepath)
 {
     std::cout << "FS::rm(" << filepath << ")\n";
-    dir_entry *blk = (dir_entry*)calloc(disk.get_no_blocks(), sizeof(dir_entry)); //new_empty block
-    dir_entry empty = {0};
-    disk.read(0, (uint8_t*)blk); //should work tm
+    dir_entry *blk = (dir_entry*)calloc(disk.get_no_blocks(), sizeof(dir_entry)); //Creates an empty block
+    disk.read(0, (uint8_t*)blk); //Reads in the root directory
     for (int i = 0; i < 64; i++){
         if (strcmp(blk[i].file_name, filepath.c_str()) == 0){
-            std::vector<int> fat_indexes = get_fats(blk[i].first_blk);
+            std::vector<int> fat_indexes = get_fats(blk[i].first_blk); //Gets where all fats of the file are stored
             for(int j = 0; j < fat_indexes.size(); j++){
-                fat[fat_indexes[j]] = 0;
+                fat[fat_indexes[j]] = 0; //Nulls those scrubs
             }
-            blk[i] = empty;
+            blk[i] = {0};
             disk.write(0, (uint8_t*)blk);
             disk.write(1, (uint8_t*)fat);
             break;
@@ -256,16 +257,16 @@ int
 FS::append(std::string filepath1, std::string filepath2)
 {
     std::cout << "FS::append(" << filepath1 << "," << filepath2 << ")\n";
-    dir_entry *blk = (dir_entry*)calloc(disk.get_no_blocks(), sizeof(dir_entry)); //new_empty block
-    disk.read(0, (uint8_t*)blk); //should work tm
+    dir_entry *blk = (dir_entry*)calloc(disk.get_no_blocks(), sizeof(dir_entry)); //Creates an empty block
+    disk.read(0, (uint8_t*)blk); //Reads in the root directory
     for (int i = 0; i < 64; i++){
         if (strcmp(blk[i].file_name, filepath1.c_str()) == 0){
             for (int j = 0; j < 64; j++){
-                if (strcmp(blk[j].file_name, filepath2.c_str()) == 0){
-                    std::string file1 = diskread(blk[i].first_blk);
-                    std::string file2 = diskread(blk[j].first_blk);
-                    diskwrite(file2.append(file1).c_str(), blk[j].first_blk);
-                    blk[j].size += blk[i].size;
+                if (strcmp(blk[j].file_name, filepath2.c_str()) == 0){ //When it finds both files
+                    std::string file1 = diskread(blk[i].first_blk); //Saves the content of file 1 and file 2
+                    std::string file2 = diskread(blk[j].first_blk); 
+                    diskwrite(file2.append(file1).c_str(), blk[j].first_blk); //Creates an entirely new file with the content of both files and starts at the same spot as file 2, not the best but it works.
+                    blk[j].size += blk[i].size; //Adds together their size
                     disk.write(0, (uint8_t*)blk);
                     break;
                 }
